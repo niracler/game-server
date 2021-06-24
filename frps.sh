c5f1b7696effdd0086fedbc7f92278c1598281a1
ubuntu <ubuntu@localhost.localdomain>
2021-06-24 22:52:46 +0800

    update port

--- frps.sh
+++ frps.sh
@@ -1,6 +1,10 @@
+docker rm -f frps >/dev/null 2>&1
+
 docker run -d \
     -p 7001:7001 \
     -p 7500:7500 \
+    -p 7777:7777 \
+    -p 25565:25565 \
     -v $(pwd)/frps/frps.ini:/conf/frps.ini \
     --restart=always \
     --name frps cloverzrg/frps-docker
